df32ab60e69c2db88d767d0835dc959003b4881a
Jonathan García <jona03g97@gmail.com>
2021-06-29 21:10:10 -0600

    Iniciando backend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const user = require("./routes/user");
+const message = require("./routes/message");
+const chat = require("./routes/chat");
 
 const app = express();
 
@@ -8,9 +11,9 @@ app.set("PORT", process.env.API_PORT || 3000);
 
 app.use(cors());
 app.use(express.json());
-app.use("/user", path.join(__dirname, "routes/user"));
-app.use("/message", path.join(__dirname, "routes/message"));
-app.use("/chat", path.join(__dirname, "routes/chat"));
+app.use("/user", user);
+app.use("/message", message);
+app.use("/chat", chat);
 
 app.listen(app.get("PORT"), () =>
   console.log(`Server on port ${app.get("PORT")}`)
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const response = require('../network/response')
-const router = express.Router()
+const express = require("express");
+const response = require("../network/response");
+const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  response.success(req, res, "Hola mundo", 200);
+});
 
-})
+router.get("/:id", (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-})
-
-router.post('/', (req, res) => {
-  
-})
-module.exports = router
+router.post("/", (req, res) => {});
+module.exports = router;
