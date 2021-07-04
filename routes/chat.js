8418b38a3a7e072dd94d88f2c0d61f81f31743bd
Jonathan García <megabyte9703@gmail.com>
2021-07-04 02:59:08 -0600

    Arreglando chats

--- routes/chat.js
+++ routes/chat.js
@@ -5,20 +5,37 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   Chat.find()
-    .then((data) => response.success(req, res, data, 200))
-    .catch((error) =>
-      response.error(req, res, "Error al traer usuario", 500, error)
-    );
-});
-
-router.get("/:id", (req, res) => {
-  Chat.findOne({ _id: req.params.id })
     .populate("users")
     .then((data) => response.success(req, res, data, 200))
     .catch((error) =>
       response.error(req, res, "Error al traer usuario", 500, error)
     );
 });
+
+// router.get("/:id", (req, res) => {
+//   Chat.find()
+//     .populate("users")
+//     .then((data) => {
+//       const newData = data.filter((chat) => {
+//         const newChat = chat.users.filter((user) => {
+//           if (user._id.toString() === req.params.id) {
+//             return chat;
+//           } else {
+//             return;
+//           }
+//         });
+//         console.log(newChat);
+//         return newChat;
+//       });
+//       return newData;
+//     })
+//     .then((data) => {
+//       response.success(req, res, data, 200);
+//     })
+//     .catch((error) =>
+//       response.error(req, res, "Error al traer usuario", 500, error)
+//     );
+// });
 
 router.post("/", (req, res) => {
   const newChat = new Chat({
